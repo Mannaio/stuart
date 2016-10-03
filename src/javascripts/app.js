6d97178561f1c903acd8839d9dacd30757d28cd5
Tommaso Villa <tommaso.villa@williamhill.com>
2016-10-03 21:35:58 +0200

    Test for Stuart Delivery

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,10 @@
 import './modules'
 
 console.log(`app.js has loaded!`)
+
+function classToggle() {
+    var el = document.querySelector("body")
+    el.classList.toggle('-menu-expanded');
+}
+document.querySelector('.nav__listItem--menu').addEventListener('click', classToggle);
+document.querySelector('.close').addEventListener('click', classToggle);
